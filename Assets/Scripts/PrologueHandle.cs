f1e46b596791fa635b3ddd2ac93bd08e9f01801c
Wieczny <robertgalonpl@gmail.com>
2018-12-03 12:06:59 +0100

    just some more things (and important: backup before therd scene

--- Assets/Scripts/PrologueHandle.cs
+++ Assets/Scripts/PrologueHandle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
+using TMPro;
+using UnityEngine.Rendering.PostProcessing;
 
 public class PrologueHandle : MonoBehaviour
 {
@@ -15,7 +17,13 @@ public class PrologueHandle : MonoBehaviour
     [Header("Scene Transforms")]
     public Transform[] firstSceneCameraTransforms;
     public Transform[] secondSceneCameraTransforms;
+    public Transform[] therdSceneCameraTransforms;
+    public PlayableDirector firstTimeline;
     public PlayableDirector secondTimeline;
+    public PlayableDirector therdTimeline;
+    public PostProcessVolume postProccess;
+    public StoryHandle storyHandler;
+    public TMP_Text storyText;
 
     private float startTimer = 0f;
     private bool[] bools = new bool[10];
@@ -39,80 +47,231 @@ public class PrologueHandle : MonoBehaviour
             return;
 
         if(!bools[0]) {
-            StartCoroutine("FirstScene");
+            StartCoroutine("BlackBegin");
         } else if(!bools[1]) {
-            StartCoroutine("SecondScene");
+            StartCoroutine("FirstScene");
         } else if(!bools[2]) {
-            //StartCoroutine("SecondScene");
+            StartCoroutine("BlackAfterFirst");
+        } else if(!bools[3]) {
+            StartCoroutine("SecondScene");
+        } else if(!bools[4]) {
+            StartCoroutine("BlackAfterSecond");
+        } else if(!bools[5]) {
+            StartCoroutine("TherdScene");
         }
 
         // firstly make visible screen
         // make screen black to change scene transform
-        
+
 
         //Debug.Log(startTimer);
     }
 
 
-    // first scene logic
-    IEnumerator FirstScene() {
-        // setup all stuff
-        cam.SetPoint(firstSceneCameraTransforms[0]);
-        RenderSettings.fog = true;
-
+    ////////////////////////////////
+    // black at beggining // blacks
+    //////////////////////////////// 
+    IEnumerator BlackBegin() {
         // we are inside
         insideCorutine = true;
+        storyHandler.gameObject.active = true;
+        storyHandler.SetStoryText("1254 - Horces Cortes", "cos tam cos tam");
+        storyHandler.transform.position += new Vector3(-25f, 0);
+        ColorGrading colorGrading = null;
+        postProccess.profile.TryGetSettings(out colorGrading);
+        colorGrading.colorFilter.value = new Color(35, 39, 255);
 
         while(!bools[0]) {
+            storyHandler.transform.position += new Vector3(0.05f, 0);
             // change screen black
-            if(startTimer >= 1f && startTimer < 3f) {
-                if(blackScreen.color.a > 0) {
-                    blackScreen.color = new Color(0, 0, 0, (2f - startTimer));
-                }
-            } else if(startTimer >= 7f && startTimer < 9f) {
-                if(blackScreen.color.a < 1) {
-                    blackScreen.color = new Color(0, 0, 0, (-7f + startTimer));
-                } else changeScene = true;
+            if(startTimer > 1f && startTimer <= 2.5f) {
+                storyHandler.SetStoryAlpha(startTimer - 1f);
+            } else if(startTimer > 7f && startTimer < 8.5f) {
+                storyHandler.SetStoryAlpha(8f - startTimer);
+            } else if(startTimer >= 8.5f) {
+                changeScene = true;
             }
-
+            
             // close coroutine when camera is near point 2
-            if(cam.ToPoint(firstSceneCameraTransforms[1], 0.005f) && changeScene) {
+            if(changeScene) {
                 insideCorutine = false;
                 changeScene = false;
                 startTimer = 0f;
                 bools[0] = true;
+                storyHandler.SetStoryText("", "");
+                storyHandler.gameObject.active = false;
             }
             yield return null;
         }
     }
 
-    // first scene logic
-    IEnumerator SecondScene() {
-        // setup all stuff
-        cam.SetPoint(secondSceneCameraTransforms[0]);
-
+    IEnumerator BlackAfterFirst() {
         // we are inside
         insideCorutine = true;
-        secondTimeline.Play();
+        storyHandler.gameObject.active = true;
+        storyHandler.transform.position += new Vector3(-25f, 0);
+        storyHandler.SetStoryText("1254 - Horces Cortes", "cos tam cos tam");
 
-        while(!bools[1]) {
+        while(!bools[2]) {
+            storyHandler.transform.position += new Vector3(0.05f, 0);
             // change screen black
-            if(startTimer >= 1f && startTimer < 3f) {
-                if(blackScreen.color.a > 0) {
-                    blackScreen.color = new Color(0, 0, 0, (2f - startTimer));
-                }
-            } else if(startTimer >= 9f && startTimer < 11f) {
-                if(blackScreen.color.a < 1) {
-                    blackScreen.color = new Color(0, 0, 0, (-9f + startTimer));
-                } else changeScene = true;
+            if(startTimer > 1f && startTimer <= 2.5f) {
+                storyHandler.SetStoryAlpha(startTimer - 1f);
+            } else if(startTimer > 7f && startTimer < 8.5f) {
+                storyHandler.SetStoryAlpha(8f - startTimer);
+            } else if(startTimer >= 8.5f) {
+                changeScene = true;
             }
 
             // close coroutine when camera is near point 2
-            if(cam.ToPoint(secondSceneCameraTransforms[1], 0.005f) && changeScene) {
+            if(changeScene) {
+                insideCorutine = false;
+                changeScene = false;
+                startTimer = 0f;
+                bools[2] = true;
+                storyHandler.SetStoryText("", "");
+                storyHandler.gameObject.active = false;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator BlackAfterSecond() {
+        // we are inside
+        insideCorutine = true;
+        storyHandler.gameObject.active = true;
+        storyHandler.transform.position += new Vector3(-240f, 0);
+        storyHandler.SetStoryText("1254 - Horces Cortes", "cos tam cos tam");
+        ColorGrading colorGrading = null;
+        postProccess.profile.TryGetSettings(out colorGrading);
+        colorGrading.colorFilter.value = new Color(86, 75, 176);
+
+        while(!bools[4]) {
+            storyHandler.transform.position += new Vector3(0.05f, 0);
+            // change screen black
+            if(startTimer > 1f && startTimer <= 2.5f) {
+                storyHandler.SetStoryAlpha(startTimer - 1f);
+            } else if(startTimer > 7f && startTimer < 8.5f) {
+                storyHandler.SetStoryAlpha(8f - startTimer);
+            } else if(startTimer >= 8.5f) {
+                changeScene = true;
+            }
+
+            // close coroutine when camera is near point 2
+            if(changeScene) {
+                insideCorutine = false;
+                changeScene = false;
+                startTimer = 0f;
+                bools[4] = true;
+                storyHandler.SetStoryText("", "");
+                storyHandler.gameObject.active = false;
+            }
+            yield return null;
+        }
+    }
+
+    ////////////////////////////////
+    // first scene logic // scenes
+    ////////////////////////////////
+    IEnumerator FirstScene() {
+        // setup all stuff
+        cam.SetPoint(firstSceneCameraTransforms[0]);
+        RenderSettings.fog = true;
+        storyText.text = "hegegedg";
+        // we are inside
+        insideCorutine = true;
+        firstTimeline.Play();
+
+        while(!bools[1]) {
+            // change screen black
+            if(startTimer < 1.5f) {
+                if(blackScreen.color.a > 0) {
+                    blackScreen.color = new Color(0, 0, 0, (1f - startTimer));
+                    storyText.color = new Color(255, 255, 255, startTimer - 0.5f);
+                }
+            } else if(startTimer >= 8f && startTimer < 9.5f) {
+                if(blackScreen.color.a < 1) {
+                    blackScreen.color = new Color(0, 0, 0, (-8f + startTimer));
+                    storyText.color = new Color(255, 255, 255, 9f - startTimer);
+                } else changeScene = true;
+            }
+            cam.ToPoint(firstSceneCameraTransforms[1], 0.005f);
+            // close coroutine when camera is near point 2
+            if(changeScene) {
                 insideCorutine = false;
                 changeScene = false;
                 startTimer = 0f;
                 bools[1] = true;
+            }
+            yield return null;
+        }
+    }
+
+
+    // second scene logic
+    IEnumerator SecondScene() {
+        // setup all stuff
+        cam.SetPoint(secondSceneCameraTransforms[0]);
+        storyText.text = "asdasdasd";
+        // we are inside
+        insideCorutine = true;
+        secondTimeline.Play();
+
+        while(!bools[3]) {
+            // change screen black
+            if(startTimer < 1.5f) {
+                if(blackScreen.color.a > 0) {
+                    blackScreen.color = new Color(0, 0, 0, (1f - startTimer));
+                    storyText.color = new Color(255, 255, 255, startTimer - 0.5f);
+                }
+            } else if(startTimer >= 8f && startTimer < 9.5f) {
+                if(blackScreen.color.a < 1) {
+                    blackScreen.color = new Color(0, 0, 0, (-8f + startTimer));
+                    storyText.color = new Color(255, 255, 255, 9f - startTimer);
+                } else changeScene = true;
+            }
+            cam.ToPoint(secondSceneCameraTransforms[1], 0.005f);
+            // close coroutine when camera is near point 2
+            if(changeScene) {
+                insideCorutine = false;
+                changeScene = false;
+                startTimer = 0f;
+                bools[3] = true;
+            }
+            yield return null;
+        }
+    }
+
+
+    // therd scene
+    IEnumerator TherdScene() {
+        // setup all stuff
+        cam.SetPoint(therdSceneCameraTransforms[0]);
+        storyText.text = "zxcvbfb";
+        // we are inside
+        insideCorutine = true;
+        therdTimeline.Play();
+
+        while(!bools[5]) {
+            // change screen black
+            if(startTimer < 1.5f) {
+                if(blackScreen.color.a > 0) {
+                    blackScreen.color = new Color(0, 0, 0, (1f - startTimer));
+                    storyText.color = new Color(255, 255, 255, startTimer - 0.5f);
+                }
+            } else if(startTimer >= 8f && startTimer < 9.5f) {
+                if(blackScreen.color.a < 1) {
+                    blackScreen.color = new Color(0, 0, 0, (-8f + startTimer));
+                    storyText.color = new Color(255, 255, 255, 9f - startTimer);
+                } else changeScene = true;
+            }
+
+            // close coroutine when camera is near point 2
+            if(changeScene) {
+                insideCorutine = false;
+                changeScene = false;
+                startTimer = 0f;
+                bools[5] = true;
             }
             yield return null;
         }
---
+++ Assets/Scripts/StoryHandle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StoryHandle : MonoBehaviour
+{
+    public TMP_Text storyName;
+    public TMP_Text storyText;
+
+
+    public void SetStoryText(string name, string text) {
+        storyName.text = name;
+        storyText.text = text;
+    }
+
+    public void SetStoryAlpha(float alpha) {
+        storyName.color = new Color(255, 255, 255, alpha);
+        storyText.color = new Color(255, 255, 255, alpha);
+    }
+}
